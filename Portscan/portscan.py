d1f494ca1b05ec62134b1bf638ad08fbd431bc69
Michael <boulevardoffakes@gmail.com>
2017-04-18 23:15:51 +0500

    Some fixes. Recognizing dns, ntp, imap now

--- Portscan/portscan.py
+++ Portscan/portscan.py
@@ -1,5 +1,6 @@
 import argparse
 import socket
+import struct
 from concurrent.futures import ThreadPoolExecutor
 
 THREADS = 512
@@ -22,15 +23,51 @@ def is_pop3(sock):
     return data[:3] == b'+OK'
 
 
+def is_imap(sock):
+    data = sock.recv(1024)
+    return data[:3] == b'* OK'
+
+
+def is_dns(host, port):
+    packet = struct.pack("HHHHHH", 12049, 256, 1, 0, 0, 0)
+    split_url = "google.com".split(".")
+    for part in split_url:
+        packet += struct.pack("B", len(part))
+        for byte in bytes(part, encoding='utf-8'):
+            packet += struct.pack("h", byte)
+    packet += struct.pack("BHH", 0, 1, 1)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(bytes(packet), (host, port))
+    data, address = sock.recvfrom(1024)
+    return data[:2] == b'\x5c\xfe' and data[2] >> 7 == 1
+
+
+def is_sntp(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    data = '\x1b' + 47 * '\0'
+    sock.sendto(data.encode(), (host, port))
+    data, address = sock.recvfrom(1024)
+    sock.close()
+    if len(data) != 48:
+        return False
+    mode = data[0] & 7
+    return mode == 4 or mode == 3
+
+
 def detect_protocol_type(host, port, is_tcp_socket=True):
-    protocols_with_tcp = {'HTTP': is_http, 'SMTP': is_smtp, 'POP3': is_pop3}
-    # protocols_with_udp = {'SNTP': is_sntp, 'DNS': is_dns} # not implemented yet
+    protocols_with_tcp = {'HTTP': is_http, 'SMTP': is_smtp, 'POP3': is_pop3, 'IMAP': is_imap}
+    protocols_with_udp = {'SNTP': is_sntp, 'DNS': is_dns}
 
     if is_tcp_socket:
         for protocol in protocols_with_tcp:
             with socket.create_connection((host, port), CONNECTION_TIMEOUT) as sock:
                 if protocols_with_tcp[protocol](sock):
                     return protocol
+    else:
+        for protocol in protocols_with_udp:
+            if protocols_with_udp[protocol](host, port):
+                return protocol
     return ''
 
 
@@ -47,8 +84,7 @@ def scan_udp(host, port):
     sock.sendto(b'', (host, port))
     try:
         data = sock.recvfrom(1024)
-        # port_type = detect_protocol_type(host, port, is_tcp_socket=False) # not implemented yet
-        port_type = ''
+        port_type = detect_protocol_type(host, port, is_tcp_socket=False)
         print('UDP {} {}'.format(port, port_type))
     except:
         pass
